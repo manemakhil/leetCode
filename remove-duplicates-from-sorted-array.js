2d766a3bfe2fa30ebce57a067838baa9ce846379
Akhil Manem <manemakhil@outlook.com>
2024-04-17 12:07:40 +0530

    used splice in remove-duplicates

--- remove-duplicates-from-sorted-array.js
+++ remove-duplicates-from-sorted-array.js
@@ -1,43 +1,21 @@
-// Problem URL: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
-
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-    let duplicates = 0;
-
-    // Loops through nums to tag duplicates
-    for (let i = 0; i < nums.length;) {
-        let j = i + 1;
-
-        if (j === nums.length) break;
-
-        while (nums[i] === nums[j]) {
-            nums[j] = 101;
-            j++;
+    // Looping thorugh the nums array
+    for (let i = 0; i < nums.length - 1;) {
+        let j = i + 1, duplicates = 0;
+        while(nums[j++] === nums[i]) {
             duplicates++;
         }
-        i = j;
+        nums.splice(++i, duplicates);
     }
-
-    /* Loops through nums to find 101 and swaps it with the next non-101 element.
-    compEnd indicates the number of 101s in-between swaps. */
-    let compEnd = 1;
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === 101) {
-            while (nums[i + compEnd] === 101) {
-                compEnd++;
-            }
-            let end = i + compEnd;
-
-            if (end === nums.length) break;
-
-            nums[i] += nums[end];
-            nums[end] = nums[i] - nums[end];
-            nums[i] -= nums[end];
-        }
-    }
-
-    return nums.length - duplicates;
+       
+    return nums.length;
 };
+
+nums = [-3,-3,-2,-1,-1,0,0,0,0,0];
+console.log(removeDuplicates(nums));
+
+console.log(nums);
