a63546e5c813aa486e6aa1a5ad1986d64f8224b9
Akhil Manem <manemakhil@outlook.com>
2026-06-10 10:21:17 +0530

    two-sum

--- two-sum.js
+++ two-sum.js
@@ -1,18 +1,20 @@
-// Problem: https://leetcode.com/problems/two-sum/
+// Problem: https://leetcode.com/problems/two-sum/description/
 
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    for(
-        let i = 0; 
-        i < nums.length - 1; 
-        i++
-    ) {
-        let otherHalfIndex = nums.indexOf(target - nums[i], i + 1);
+var twoSum = function (nums, target) {
+    const otherNumHash = {}
 
-        if(otherHalfIndex !== -1) return [i, otherHalfIndex];
+    for (let thisIndex = 0; thisIndex < nums.length; thisIndex++) {
+        const thisNum = nums[thisIndex];
+
+        const otherNum = target - thisNum;
+        const matchingFirstIndex = otherNumHash[otherNum];
+
+        if (matchingFirstIndex !== undefined) return [matchingFirstIndex, thisIndex];
+        otherNumHash[thisNum] = thisIndex;
     }
 };
